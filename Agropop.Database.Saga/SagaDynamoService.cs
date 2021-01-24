e6512285e15d94684fc089ddecab49e0fbe27dba
Michelle Rodrigues <mica.msr@gmail.com>
2021-01-24 07:41:27 -0300

    configuração DynamoDB para pegar mensgem

---
+++ Agropop.Database.Saga/SagaDynamoService.cs
@@ -0,0 +1,52 @@
+﻿using Agropop.Database.Saga.Tables;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using System.Threading.Tasks;
+
+namespace Agropop.Database.Saga
+{
+    public class SagaDynamoService
+    {
+        private static AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+
+        public async static Task<string> BuscarMensagemAgendamento(string msgId)
+        {
+            DynamoDBContext context = new DynamoDBContext(client);
+
+            var retorno = await GetSagaMessage(context, msgId);
+
+            return retorno.BodyMessage;
+        }
+
+        public async static Task<bool> IncluirMensagemAgendamento(string msg, string msgId)
+        {
+            DynamoDBContext context = new DynamoDBContext(client);
+                        
+            return await PutSagaMessage(context, msg, msgId);
+        }
+
+
+        private async static Task<SagaMessageTable> GetSagaMessage(DynamoDBContext context, string msgId)
+        {
+            SagaMessageTable sagaItem = await context.LoadAsync<SagaMessageTable>(msgId).ConfigureAwait(false);
+
+            return sagaItem;
+        }
+
+        private async static Task<bool> PutSagaMessage(DynamoDBContext context, string msg, string msgId)
+        {
+            
+            SagaMessageTable sampleTableItems = new SagaMessageTable
+            {
+                BodyMessage = msg,
+                Id = msgId
+            };
+
+            var batchWrite = context.CreateBatchWrite<SagaMessageTable>();
+
+            batchWrite.AddPutItem(sampleTableItems);
+
+            return true;
+        }
+    }
+}
---
+++ Agropop.Database.Saga/Tables/SagaMessageTable.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+
+namespace Agropop.Database.Saga.Tables
+{
+    [DynamoDBTable("AgroPopSagaColletion")]
+    public class SagaMessageTable
+    {
+        [DynamoDBHashKey] //Partition key
+        public string Id
+        {
+            get; set;
+        }
+        
+        // Properties included implicitly.
+        public string BodyMessage
+        {
+            get; set;
+        }
+    }
+}
